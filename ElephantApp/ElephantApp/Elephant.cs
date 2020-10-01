ae549492d0f1c0da0253a9652bb667f50d0cd0c6
Isaiah <dothackdasen@gmail.com>
2020-09-30 22:39:06 -0700

    Building classes

--- ElephantApp/ElephantApp/Elephant.cs
+++ ElephantApp/ElephantApp/Elephant.cs
@@ -6,5 +6,12 @@ namespace ElephantApp
 {
     class Elephant
     {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
     }
 }
