c82c584f7f309ededa0864f27b79e6ad16ccafa2
Isaiah <dothackdasen@gmail.com>
2020-09-30 22:27:18 -0700

    Elephant App added

---
+++ ElephantApp/ElephantApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElephantApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
