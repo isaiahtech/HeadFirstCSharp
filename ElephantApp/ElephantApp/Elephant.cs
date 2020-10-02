3e7efb04a7f54272b7d954625627cb6285eda072
Isaiah <dothackdasen@gmail.com>
2020-10-01 22:35:43 -0700

    addit class refactor

--- ElephantApp/ElephantApp/Elephant.cs
+++ ElephantApp/ElephantApp/Elephant.cs
@@ -13,5 +13,16 @@ namespace ElephantApp
             Console.WriteLine("My name is " + Name + ".");
             Console.WriteLine("My ears are " + EarSize + " inches tall.");
         }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
     }
 }
