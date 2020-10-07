3ba3e130b88c5f67079fdbd4594f11cafc773340
Isaiah <dothackdasen@gmail.com>
2020-10-06 22:12:31 -0700

    New random app

---
+++ RandomTestDrive/RandomTestDrive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomTestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomInt = random.Next();
+            Console.WriteLine(randomInt);
+        }
+    }
+}
