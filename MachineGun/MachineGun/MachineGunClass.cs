b4c65cf53e1e9e7a7414e8eaba5e2994031d0f20
Isaiah <dothackdasen@gmail.com>
2020-11-02 22:51:01 -0800

    Bullet tracking console program added

---
+++ MachineGun/MachineGun/MachineGunClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class MachineGunClass
+{
+	public const int MAGAZINE_SIZE = 16;
+
+	private int bullets = 0;
+	private int bulletsLoaded = 0;
+
+	public int GetBulletsLoaded() { return bulletsLoaded; }
+
+	public bool IsEmpty() { return bulletsLoaded == 0; }
+
+	public int GetBullets() { return bullets; }
+
+	public void SetBullets(int numberOfBullets)
+    {
+		if (numberOfBullets > 0)
+			bullets = numberOfBullets;
+		Reload();
+    }
+
+	public void Reload()
+    {
+		if (bullets > MAGAZINE_SIZE)
+			bulletsLoaded = MAGAZINE_SIZE;
+		else
+			bulletsLoaded = bullets;
+    }
+
+	public bool Shoot()
+    {
+		if (bulletsLoaded == 0) return false;
+		bulletsLoaded--;
+		bullets--;
+		return true;
+    }
+	
+}
---
+++ MachineGun/MachineGun/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MachineGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MachineGunClass gun = new MachineGunClass();
+            while (true)
+            {
+                Console.WriteLine($"{gun.GetBullets()} bullets, {gun.GetBulletsLoaded()} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.SetBullets(gun.GetBullets() + MachineGunClass.MAGAZINE_SIZE);
+                else if (key == 'q') return;
+            }
+        }
+    }
+}
