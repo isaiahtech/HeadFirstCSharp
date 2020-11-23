2702fc05641ea904de73b1b58362e88fcd5a5fcd
Isaiah <dothackdasen@gmail.com>
2020-11-22 21:00:50 -0800

    Fixed damage, corrected xaml class title

--- SwordDamageGUI/SwordDamageGUI/MainWindow.xaml.cs
+++ SwordDamageGUI/SwordDamageGUI/MainWindow.xaml.cs
@@ -21,13 +21,12 @@ namespace swordDamage
     public partial class MainWindow : Window
     {
         Random random = new Random();
-        SwordDamage swordDamage = new SwordDamage();
+        SwordDamage swordDamage;
         public MainWindow()
         {
             InitializeComponent();
-            swordDamage.SetMagic(false);
-            swordDamage.SetFlaming(false);
-            RollDice();
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+            DisplayDamage();
         }
 
         public void RollDice()
@@ -38,7 +37,7 @@ namespace swordDamage
 
         void DisplayDamage()
         {
-            damage.Text = "Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP";
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -48,25 +47,25 @@ namespace swordDamage
 
         private void Flaming_Checked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetFlaming(true);
+            swordDamage.Flaming = true;
             DisplayDamage();
         }
 
         private void Flaming_Unchecked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetFlaming(false);
+            swordDamage.Flaming = false;
             DisplayDamage();
         }
 
         private void Magic_Checked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetMagic(true);
+            swordDamage.Magic = true;
             DisplayDamage();
         }
 
         private void Magic_Unchecked(object sender, RoutedEventArgs e)
         {
-            swordDamage.SetMagic(false);
+            swordDamage.Magic = false;
             DisplayDamage();
         }
 
